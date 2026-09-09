817a539cadd31ee5874c070573a2ef7f804a6697
edgm2003 <54037279+edgm2003@users.noreply.github.com>
2026-09-09 01:04:14 +0000

    Conectando el frontend con el backend y guardando últimos cambios

--- src/app.py
+++ src/app.py
@@ -16,8 +16,7 @@ from api.mail import mail
 from datetime import timedelta
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../dist/')
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
